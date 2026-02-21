1048191e9cf7bd444b87cf637adae6f54747a74c
Nicolas Javier Agostino <nico.agostino@hotmail.com>
2026-02-21 17:33:47 -0300

    Commit inicial del proyecto

---
+++ src/main/java/com/aluracursos/libroinfo/LibroinfoApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.libroinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/libroinfo/LibroinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.libroinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
